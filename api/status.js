c0e0a479e77737b8406460384cb1bd090f56ee5c
DINSTORE99 <dinstoree41@gmail.com>
2026-08-07 05:06:39 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,6 +1,21 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    URL_BACKEND_API: process.env.URL_BACKEND_API ?? "TIDAK ADA",
-    NODE_ENV: process.env.NODE_ENV
-  });
+export default async function handler(req, res) {
+  try {
+    const backendUrl = process.env.URL_BACKEND_API;
+
+    const response = await fetch(`${backendUrl}/api/status`);
+
+    const data = await response.json();
+
+    return res.status(response.status).json(data);
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      name: error.name,
+      message: error.message,
+      cause: error.cause
+    });
+  }
 }
