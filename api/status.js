90999ec5de76d5b329b15f3cb8667080eb355d53
DINSTORE99 <dinstoree41@gmail.com>
2026-08-08 21:53:22 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,40 +1,21 @@
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({
-      success: false,
-      message: "Method tidak diizinkan"
-    });
-  }
-
   try {
     const backendUrl = process.env.BACKEND_URL_API;
 
-    if (!backendUrl) {
-      return res.status(500).json({
-        success: false,
-        message: "BACKEND_URL_API belum diatur"
-      });
-    }
-
-    const response = await fetch(
-      `${backendUrl}/api/status`,
-      {
-        method: "GET",
-        cache: "no-store"
-      }
-    );
+    const response = await fetch(`${backendUrl}/api/status`);
 
     const data = await response.json();
 
     return res.status(response.status).json(data);
 
   } catch (error) {
-    console.error("STATUS API ERROR:", error);
+    console.error(error);
 
     return res.status(500).json({
       success: false,
-      message: "Gagal menghubungi backend",
-      error: error.message
+      name: error.name,
+      message: error.message,
+      cause: error.cause
     });
   }
 }
