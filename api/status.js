4f1ce217fc444edcfc32939d8142885f0bdb762b
DINSTORE99 <dinstoree41@gmail.com>
2026-08-08 21:55:09 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,21 +1,22 @@
 export default async function handler(req, res) {
-  try {
-    const backendUrl = process.env.BACKEND_URL_API;
+try {
+const backendUrl = process.env.URL_BACKEND_API;
 
-    const response = await fetch(`${backendUrl}/api/status`);
+const response = await fetch(`${backendUrl}/api/status`);  
 
-    const data = await response.json();
+const data = await response.json();  
 
-    return res.status(response.status).json(data);
+return res.status(response.status).json(data);
 
-  } catch (error) {
-    console.error(error);
+} catch (error) {
+console.error(error);
 
-    return res.status(500).json({
-      success: false,
-      name: error.name,
-      message: error.message,
-      cause: error.cause
-    });
-  }
+return res.status(500).json({  
+  success: false,  
+  name: error.name,  
+  message: error.message,  
+  cause: error.cause  
+});
+
+}
 }
